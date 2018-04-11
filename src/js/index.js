9dc407c0b624ddcddf08650d0edb377473e89214
domin <dominik.polak@green-light.com>
2018-04-11 23:44:02 +0200

    Add form validation. Update assets. Update content.

--- src/js/index.js
+++ src/js/index.js
@@ -2,25 +2,31 @@
   $(document).ready(() => {
     const logo = $('#logo');
     const btnReady = $('#btn-ready');
-    let slideInterval = null;
+    const contactForm = $('#contact-form');
+    const autoSlide = function() {
+      clearTimeout($.fn.fullpage.sliderTimeout);
+      $.fn.fullpage.sliderTimeout = setTimeout(() => {
+        $.fn.fullpage.moveSlideRight();
+      }, 7e3);
+    };
 
     $.get('/assets/img/logo.svg', response => {
       logo.html(jQuery(response).find('svg'));
     }, 'xml');
 
-    $('#contact-form').submit(e => {
+    contactForm.submit(e => {
       e.preventDefault();
 
       $.ajax({
         type: 'POST',
         contentType: 'application/json',
         url: 'https://api.dev.quest.gl/marketing-data/contacts',
-        data: JSON.stringify({ email: $('#user-input-email').val() }),
+        data: JSON.stringify({ email: contactForm.find('#user-input-email').val() }),
         dataType: 'json',
         success: () => {
           $('#output-message-heading').text('Thank you');
           $('#output-message').text('for submitting your email');
-          $('#contact-form').hide();
+          contactForm.hide();
         },
         error: () => {
           $('#output-message-heading').text('Sorry');
@@ -35,8 +41,8 @@
       slidesNavPosition: 'bottom',
       controlArrows: false,
       paddingTop: '55px',
+      onSlideLeave: autoSlide,
       onLeave: (index, nextIndex) => {
-        clearInterval(slideInterval);
         logo.removeClass();
         btnReady.removeClass();
 
@@ -49,20 +55,12 @@
           btnReady.addClass('btn-link');
         } else if (nextIndex === 2 || nextIndex === 4) {
           btnReady.addClass('btn-primary');
+          logo.addClass('black');
         } else if (nextIndex !== 3) {
           btnReady.addClass('hidden');
-        }
-
-        if (nextIndex === 3) {
-          slideInterval = setInterval(() => {
-            $.fn.fullpage.moveSlideRight();
-          }, 5e3);
-        }
-
-        if (nextIndex === 2 || nextIndex === 4) {
-          logo.addClass('black');
         } else if (nextIndex === 3) {
           logo.addClass('white');
+          autoSlide();
         }
       },
     });
